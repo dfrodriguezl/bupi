632ce5cae31e0a1c01fba90c4a4e5b3744046961
Ivan Carrillo - Personal PC <ivancho4321@gmail.com>
2020-10-14 01:38:25 -0500

    cambios 8

--- backend/server.js
+++ backend/server.js
@@ -296,7 +296,9 @@ var DIST_DIR = path.join(__dirname, "../dist/");
 
 app.use("/", expressStaticGzip(DIST_DIR));
 
-
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
 
   //backend en el puerto 3000
