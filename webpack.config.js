48f57d89c88d381153a5fff6dd85a3049fead381
Ivan Carrillo - Personal PC <ivancho4321@gmail.com>
2020-10-14 01:58:03 -0500

    cambios 10

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     //publicPath: '/',
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    publicPath: "/bienes-raices"
+    publicPath: "/"
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
