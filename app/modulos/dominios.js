b392c7d851bbc3c829d6297dc831429d4b0a962e
Diego Fernando Rodriguez Lamus <dfrodriguezl@dane.gov.co>
2021-07-31 21:28:35 -0500

    Carga de dominios masivos

--- app/modulos/dominios.js
+++ app/modulos/dominios.js
@@ -6,7 +6,7 @@ import Popup from 'reactjs-popup';
 import { useForm } from "react-hook-form";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import XLSX from 'xlsx'
 
 
 const Dominios = () => {
@@ -30,17 +30,6 @@ const Dominios = () => {
 
         getDominios();
     },[])
-
-
-
-//   const download=(data) => {
-//     setShow(true)
-//     servidorDocs('/excel', data).then(response => {
-//         console.log(response)
-//     //   setShow(false)
-//     })
-
-//   }
 
   const clickDescarga = () => {
       let data = {
@@ -67,15 +56,70 @@ const Dominios = () => {
         });
     }
 
-    const TablaValores = ({values}) => {
+    const TablaValores = ({values,domi}) => {
+
+        const upload = e=> {
+        
+            const files = e.target.files
+            let f = files[0];
+            let reader = new FileReader();
+            reader.onload = function(e){
+                const bstr = e.target.result;
+                const wb = XLSX.read(bstr,{type:'binary'});
+                const wsname = wb.SheetNames[0];
+                const ws = wb.Sheets[wsname];
+                const data = XLSX.utils.sheet_to_json(ws,{header:0});
+                // console.log(data);
+                data.map((d) => {
+                    const datos = {
+                        dominio: d.dominio,
+                        valor: d.valor,
+                        descripcion: d.descripcion
+                    }
+
+                    createValue(datos,"insert_valor_dominios")
+                    // console.log(d)
+                })
+
+                getValores(domi)
+                // servidorPost('/backend',)
+            }
+
+            reader.readAsBinaryString(f);
+            // setFilename(id + '-' + cod+'.pdf')
+    
+            // const formData = new FormData()
+        
+            // formData.append("file",files[0],id + '-' + cod+'.pdf')
+    
+            // servidorPost('/upload/' + id+'-'+cod, formData).then((response) => {
+            //     console.log(response)
+            //     if (response.status==200) {
+            //         toast.success("Documento cargado al sistema: " + titulo);
+            //         setRefresh(!refresh)
+            //     } else {
+            //         toast.error("Problema al cargar documento");
+                    
+            //     }
+            // })
+    
+        }
 
 
         return (
             <Fragment>
                 <div>
                     <ModalCreate open={<button style={{display: "inline"}}>Crear valor</button>}></ModalCreate>
-                    <button style={{display: "inline", float: 'right', marginRight: 10}} onClick={clickDescarga}>Descargar valores (csv)</button>
-                    <button style={{display: "inline", float: 'right', marginRight: 10}}>Actualizar dominios masivo (csv)</button>
+                    <div className="upload_file" style={{display: "inline", float: 'right', marginRight: 10}}>
+                        <div >
+                            <label htmlFor="domains" className="label-input" >Actualizar dominios masivo (xlsx)
+                                <input type="file" id="domains" className="input" accept=".csv,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" onChange={upload}/> 
+                            </label> 
+                        </div>
+                    </div>
+                    <button style={{display: "inline", float: 'right', marginRight: 10}} onClick={clickDescarga}>Descargar valores (xlsx)
+                        <input type="file" id="file_domains"  className="input" /> 
+                    </button>
                 </div>
                 <br/>
                 <div id="documentos">            
@@ -89,10 +133,6 @@ const Dominios = () => {
                             <p>{e.valor}</p>
                             <p>{e.descripcion}</p>
                             <ModalForm open={<EditIcon/>} valor={e.valor} descripcion={e.descripcion} dominio={e.dominio}></ModalForm>
-                        {/* <p>{e.usuario}</p>
-                        <p>{e.fecha}</p>
-                        <Modal  nombre={e.nombre} refresh={setRefresh}/>
-                        <FindInPageIcon onClick={()=>download(e.id)}></FindInPageIcon> */}
                         </div>
                   ))}
                  </div>
@@ -105,7 +145,7 @@ const Dominios = () => {
     const ModalForm = ({open,valor,descripcion}) => {
     
         const submitFormEdit = (datos) => { 
-            createValue(datos,"update_valor_dominios")
+            createValue(datos,"insert_valor_dominios","ind")
         }
     
         return (
@@ -154,7 +194,7 @@ const Dominios = () => {
     
         
         const submitFormCreate = (datos) => { 
-            createValue(datos,"insert_valor_dominios")
+            createValue(datos,"insert_valor_dominios","ind")
         }
     
     
@@ -200,7 +240,7 @@ const Dominios = () => {
         )
     }
 
-    const createValue = (datos,consulta) => {
+    const createValue = (datos,consulta,tipo) => {
         
         console.log(datos)
         var data = {
@@ -214,7 +254,10 @@ const Dominios = () => {
         servidorPost('/backend', data).then((response) => { 
             if (response.data.length > 0) {
                 toast.success("Registro creado satisfactoriamente");
-                getValores(datos.dominio)
+                if(tipo != null){
+                    getValores(datos.dominio)
+                }
+                
             }else{
                 toast.error("Ya existe el registro")
             }
@@ -242,7 +285,7 @@ const Dominios = () => {
                 />
                 <br/>
                 {dominio!=""?
-                    <TablaValores values={valores}/>
+                    <TablaValores values={valores} domi={dominio}/>
                     :null}
             </div>
             <ToastContainer/>
