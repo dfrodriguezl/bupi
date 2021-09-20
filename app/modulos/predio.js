5e411c708b6ae9fe20a70db8fadbb714f324ff9b
Diego <dfrodriguezl94@gmail.com>
2021-09-19 20:23:59 -0500

    Reporte de validación

--- app/modulos/predio.js
+++ app/modulos/predio.js
@@ -45,7 +45,7 @@ const gestionPermisos = (index) => {
     } else if (index == 19) {
         tipo_permiso = [6, 7];
     }
-    else if ([2, 3, 4, 7, 10, 11, 14].includes(index)) {
+    else if ([2, 3, 4, 7, 9, 10, 11, 14, 18].includes(index)) {
 
         tipo_permiso = [6];//editar formulario técnico  
     } else if ([5, 6, 8, 15, 16, 20, 21, 22].includes(index)) {
