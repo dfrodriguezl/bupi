1c5cb9af4859ed9789a02b4a5ba471239230c434
Diego <dfrodriguezl94@gmail.com>
2022-05-21 19:01:42 -0500

    Deploy aa

--- app/js/request.js
+++ app/js/request.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 // const destino = "https://www.acueducto.com.co/depuracionpredial/bienes-raices";
 // const destino = "http://192.168.56.10/bienes-raices";
 //const destino="https://nowsoft.app/bienes-raices";
-const destino = "http://172.19.26.105/bienes-raices";
+const destino = "http://bupi.invias.col/bienes-raices";
 
 export function servidorPost(uri,datos){
 
